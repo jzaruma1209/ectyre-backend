1d03371a0d96ad3fca0204de9552380b302db46a
jzaruma1209 <jzaruma1209@gmail.com>
2026-04-01 16:02:29 -0500

    advances

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,12 @@ module.exports = {
   development: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
   },
   test: {
